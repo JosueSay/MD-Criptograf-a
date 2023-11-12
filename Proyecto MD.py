e2e0ff541f01a43ffc9be27d20809ef4adf5a0f6
JosueSay <106031855+JosueSay@users.noreply.github.com>
2023-11-12 15:30:24 -0600

    Finalizacion - Método Vigénere

--- Proyecto MD.py
+++ Proyecto MD.py
@@ -4,9 +4,8 @@
 diccionario = "ABCDEFGHIJKLMNOPQRSTUVWXYZ" # alfabeto a usar
 modulo = len(diccionario) # modulo segun el alfabeto
 
-
 # Función para descifrar mensajes usando el sistema Vigénere
-def dameMensajeVigenere(_clave, _llave):
+def dameMensajeVigenere(_clave, _llave, _esCifrado):
     clave = _clave # clave del mensaje a descifrar
     llave = _llave # llave del mensaje a descifrar
     
@@ -14,38 +13,51 @@ def dameMensajeVigenere(_clave, _llave):
     codigo_llave = [diccionario.find(caracter) for caracter in separacion_llave] # codigo de los caracteres de separacion_llave
 
     bloques_mensaje = dameBloques(clave, llave) # mensaje a descifrar separado por bloques
-    vector_codigo = []
-    vector_cifrado = []
-
+    vector_codigo = [] # lista con el codigo de la clave
+    
     # obtener el vector codigo de los bloques
     for i in bloques_mensaje:
         separacion = list(i)
         for j in separacion:
             vector_codigo.append(diccionario.find(j))
             
-           
-
+    vector_cifrado = dameCodigoBloques(codigo_llave, vector_codigo, _esCifrado) # lista con el codigo resultante de la operacion
+    mensaje_respuesta = [diccionario[i] for i in vector_cifrado] # mensaje segun el vector_cifrado
+    
+    return mensaje_respuesta
 
 # Función para operar el codigo de la llave con el codigo de los bloques
-def dameCodigoBloques(_codigo_llave, _vector_codigo):
-   
-    _vector_cifrado = []
-   # Calcular la longitud de vector_sumando para usarla en el módulo
-    longitud_sumando = len(_codigo_llave)
+def dameCodigoBloques(_codigo_llave, _vector_codigo, _esCifrado):
+    vector_suma = [] # vector resultante
+    longitud_llave = len(_codigo_llave) # calcular la longitud de _codigo_llave para usarla en el módulo
 
-    # Iterar sobre vector_codigo y sumar modularmente con vector_sumando
-    for i, valor_codigo in enumerate(_vector_codigo):
-        valor_sumando = _codigo_llave[i % longitud_sumando]
-        resultado_modular = (valor_codigo + valor_sumando) % modulo
-        _vector_cifrado.append(resultado_modular) 
+    # realizar la operación para cifrado
+    if _esCifrado:
+        if longitud_llave > 0:
+            # Iterar sobre vector_codigo y sumar modularmente con _codigo_llave
+            for i, valor_codigo in enumerate(_vector_codigo):
+                valor_sumando = _codigo_llave[i % longitud_llave]
+                resultado_modular = (valor_codigo + valor_sumando) % modulo
+                vector_suma.append(resultado_modular)
+        else:
+            print("Error: La longitud de _codigo_llave debe ser mayor que cero.")
     
-    return _vector_cifrado
+    # realizar la operación para descifrado
+    else:
+        if longitud_llave > 0:
+            # Iterar sobre vector_codigo y sumar modularmente con _codigo_llave
+            for i, valor_codigo in enumerate(_vector_codigo):
+                valor_sumando = _codigo_llave[i % longitud_llave]
+                resultado_modular = (valor_codigo - valor_sumando) % modulo
+                vector_suma.append(resultado_modular)
+        else:
+            print("Error: La longitud de _codigo_llave debe ser mayor que cero.")
+
+    return vector_suma
 
 # Función para obtener bloques de la clave segun la llave
 def dameBloques(_clave, _llave):
-    
-    _bloques_mensaje = []
-    
+    _bloques_mensaje = [] # lista de bloques separada por bloques  
     bloques_completos = esBloquesCompletos(_clave, _llave) # determinar si la lista hay que agregar letras dummys o nos
 
     # realizar separación normal
@@ -54,7 +66,7 @@ def dameBloques(_clave, _llave):
             bloque = _clave[i:i+len(_llave)]
             _bloques_mensaje.append(bloque)
         
-        print(_bloques_mensaje)
+        print("Los bloques son:", _bloques_mensaje)
         
     # agregar letras dummys al final
     else:
@@ -68,7 +80,7 @@ def dameBloques(_clave, _llave):
         while letras_extras > 0:
             _bloques_mensaje[-1] = _bloques_mensaje[-1] + "X"
             letras_extras -= 1  
-        print(_bloques_mensaje)
+        print("Los bloques son:", _bloques_mensaje)
 
     return _bloques_mensaje
 
@@ -88,21 +100,32 @@ def esBloquesCompletos(_clave, _llave):
         print("Error: División por cero no permitida.")
         return False
 
+# Función para pedir la información
+def pedirInfo():
+    while True:
+        esCifrado = input("Ingresa la opción que deseas: \n1- Cifrado\n2- Descifrado\n")
+        
+        try:
+            esCifrado = int(esCifrado)
+        except ValueError:
+            print("Error: Debes ingresar un número entero (1 o 2).")
+            continue
 
+        if esCifrado in [1, 2]:
+            break
+        else:
+            print("Error: Debes ingresar 1 o 2.")
 
+    clave = input("Ingresa el mensaje: ")
+    llave = input("Ingresa la llave: ")
 
-    
-    
+    if esCifrado == 1:
+        mensaje = dameMensajeVigenere(clave, llave, True)
+    else:
+        mensaje = dameMensajeVigenere(clave, llave, False)
 
+    cadena = ''.join(mensaje)
+    print("El mensaje procesado es:", cadena)
 
-
-
-
-
-
-
-
-
-
-
-
+# Llama a la función
+pedirInfo()
